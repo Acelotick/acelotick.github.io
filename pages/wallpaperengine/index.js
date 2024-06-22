c90fc90a0a742335e9a41ee6fddb4711da1455e5
Acelotick <vitalijkarpov.jag@gmail.com>
2024-06-23 04:07:07 +0500

    Update 23.06.24

--- pages/wallpaperengine/index.js
+++ pages/wallpaperengine/index.js
@@ -1,1 +1,17 @@
 let d=(file)=>{let a=document.createElement('a');a.href='./files/'+file;a.download=file;document.body.appendChild(a);a.click();document.body.removeChild(a)}
+
+let g = e => document.getElementById(e)
+
+g('we').onclick = function() {
+    window.location.href='https://github.com/Acelotick/acelotick.github.io/raw/main/pages/wallpaperengine/files/Wallpaper%20Engine.zip'
+}
+
+g('weg').onclick = function() {
+    window.location.href='./guide/index.html'
+}
+
+for (v of [1, 2, 3, 4, 5, 6, 7, 8]) {
+    g('bg' + v).onclick = function() {
+        d(v + '.zip')
+    }
+}
